6e8e4013d53b0929ca08d08fb355ebe4af476082
RobRas <bobbyras90@gmail.com>
2016-06-29 16:25:40 -0700

    Created some C++ classes for our tank and player controller.

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+
+
+
+ATank* ATankPlayerController::GetControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not controlling a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Posessed tank: %s"), *ControlledTank->GetName());
+	}
+}
